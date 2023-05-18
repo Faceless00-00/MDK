5639238daa2ae3c753b30999a895e30d3d640d99
Faceless00-00 <118180292+Faceless00-00@users.noreply.github.com>
2023-05-18 15:32:52 +0300

    Add files via upload

---
+++ suvorov_5_laba/Suvorov_5_practich.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suvorov_5_pract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            a = Convert.ToString(Console.ReadLine());
+            for (int i = 0; i < a.Length; i++)
+            {      
+                if (a[i] == '*')
+                {
+                    i++;
+                    continue;
+                }
+                Console.Write(a[i]);
+            }
+        }
+    }
+}
