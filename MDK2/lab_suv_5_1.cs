997367ce76c6a6e3c69a2b7cc69dc6aebc385619
Faceless00-00 <118180292+Faceless00-00@users.noreply.github.com>
2023-05-22 12:57:38 +0300

    Add files via upload

---
+++ MDK2/lab_suv_5_1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suvorov5lab_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            int i;
+            a = Console.ReadLine();
+            b = "";
+            for (i = 0; i < a.Length; i++)
+            {
+                if (a[i] != '*')
+                {
+                    b += a[i];
+                }
+                else 
+                {
+                    b += "";
+                }
+            }
+            for (i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i]);
+            }
+        }
+    }
+}
---
+++ MDK2/suvorov_lab5_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_suv_5_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n = 2;
+            string a, b= "";
+            a = Console.ReadLine();
+            string[] mas = a.Split(' ');
+            for (i = 2; i < mas.Length; i++)
+            {
+                mas[i] = " ";
+            }
+            for (i = 0; i < mas.Length; i++)
+            {
+                b += mas[i];
+                b += ' ';
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
